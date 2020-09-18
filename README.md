59cc3ee1300bdc30072a52f2e72634f95f1c110c
spracs <qkcomer@gmail.com>
2020-09-18 14:31:05 +0300

    test-app

--- README.md
+++ README.md
@@ -16,3 +16,13 @@ Host target-host-name
 bastion_IP = 35.208.230.134
 someinternalhost_IP = 10.128.0.3
 
+
+testapp_IP = 35.228.152.217
+testapp_port = 9292
+
+
+gcloud compute instances create reddit-app --boot-disk-size=10GB --image-family ubuntu-1604-lts --image-project=ubuntu-os-cloud --machine-type=g1-small --tags puma-server --restart-on-failure --metadata startup-script-url=gs://sdv-buck1/startup_script.sh
+
+gcloud compute firewall-rules create open-tcp-9292 --allow=tcp:9292 --target-tags=puma-server
+
+
---
+++ startup_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+puma -d
+
