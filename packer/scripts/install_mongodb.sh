5b4963894fee9096969c93985626e306c95a9b6e
spracs <qkcomer@gmail.com>
2020-09-24 13:15:47 +0300

    laba packer

---
+++ packer/scripts/install_mongodb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -qO - http://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+apt-get update
+apt-get install -y mongodb-org
+systemctl start mongod
+systemctl enable mongod
+
