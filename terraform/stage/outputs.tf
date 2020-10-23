e437529b42fd451c0623b244a1049d3a3cfb1878
spracs <qkcomer@gmail.com>
2020-10-23 16:47:32 +0300

    One playbook, some plays

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,6 +4,9 @@ output "app_external_ip" {
 output "db_external_ip" {
   value = module.db.db_external_ip
 }
+output "db_internal_ip" {
+  value = module.db.db_internal_ip
+}
 
 #output "lb_external_ip" {
 #  value = google_compute_global_forwarding_rule.my_lb.ip_address
